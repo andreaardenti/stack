863f37c050a39d01165925a9310f8622bd03ebc6
Carlo Leonardi <carloleonardi83@gmail.com>
2018-09-17 12:54:38 +0200

    add samples

--- Main.java
+++ Main.java
@@ -3,13 +3,19 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-        Stack s = new Stack(5);
-        s.push(14);
+        Stack s = new Stack(10);
+        /*s.push(14);
         s.push(34);
         s.push(17);
         s.pop();
         s.push(32);
         s.push(11);
         s.push(9);
+
+        s.shift(12);
+
+        s.unshift(40);*/
+        int y = s.getMin();
+        int z = s.getMax();
     }
 }
--- Stack.java
+++ Stack.java
@@ -4,6 +4,9 @@ public class Stack {
 
     public Stack(int size) {
         this.array = new int[size];
+        for(int i=0; i < this.array.length-1; i++) {
+            this.array[i] = (int) (Math.random() * 100);
+        }
     }
 
     public void push(int x) {
@@ -23,4 +26,39 @@ public class Stack {
     public boolean isFull(){
         return this.level == this.array.length;
     }
+
+    public void shift(int newValue) {
+        for(int i = array.length - 1; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+        array[0] = newValue;
+    }
+
+    public void unshift(int newValue) {
+        for(int i=0; i < array.length-1; i++){
+            array[i] = array[i+1];
+        }
+        array[array.length-1] = newValue;
+    }
+
+    public int getMin(){
+        int min = this.array[0];
+        for (int i=1; i < this.array.length; i++) {
+            if (min > this.array[i]) {
+                min = this.array[i];
+            }
+        }
+        return min;
+    }
+
+    public int getMax(){
+        int max = this.array[0];
+        for (int i=1; i < this.array.length; i++) {
+            if (max < this.array[i]) {
+                max = this.array[i];
+            }
+        }
+        return max;
+    }
+
 }
