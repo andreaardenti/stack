24c48a8e72f8f932ead0ac0e701408ed245456c3
Andrea Ardenti <andreaardenti@users.noreply.github.com>
2018-09-19 15:17:35 +0200

    Update CartesianPlan.java

--- CartesianPlan.java
+++ CartesianPlan.java
@@ -4,26 +4,32 @@ public class CartesianPlan {
 
     public CartesianPlan(int size) {
         this.array = new int[size][2];
-        for (int i=0; i< array.length; i++) {
+        for (int i=0; i<array.length; i++) {
             this.array[i][0] =(int) (Math.random() * 100);
             this.array[i][1] =(int) (Math.random() * 100);
         }
     }
 
     public void shift(int x, int y) {
+        // scorro l'array
         for(int i = array.length - 1; i > 0; i--) {
             array[i] = array[i - 1];
         }
-        int[] newPosition= new int[2];
+        //associo ad una nuova variabile newPosition di tipo array le nuove posizioni di "x" e "y"
+        int[] newPosition = new int[2];
         newPosition[0] = x;
         newPosition[1] = y;
+
+        //assegno alla posizione 0 dell'array iniziale le due nuove coordinate di "x" e "y"
         array[0] = newPosition;
     }
 
     public int getNear() {
+        //calcolo col teorema di pitagora la media di x e y
         double distance = Math.sqrt(Math.pow(this.array[0][0], 2) + Math.pow(this.array[0][1], 2));
         int index = 0;
         for (int i=1; i < this.array.length; i++) {
+            //confronto di ogni valore iesimo dell'array
             double distanceIesime =  Math.sqrt(Math.pow(this.array[i][0], 2) + Math.pow(this.array[i][1], 2));
             if (distanceIesime < distance) {
                 index = i;
@@ -33,22 +39,20 @@ public class CartesianPlan {
         return index;
     }
 
-
     public void sort() {
         int[][] sortedArray = new int[this.array.length][2];
         for (int i=0; i < this.array.length; i++) {
             int nearPosition = this.getNear();
             sortedArray[i] = this.array[nearPosition];
 
+            //creo un array "big" con i valori più alti di x e y
             int[] big = new int[2];
             big[0] = 100;
             big[1] = 100;
-            //sovrascrivo la posizione presa con una più grande.
 
+            //sovrascrivo la posizione presa con una più grande.
             this.array[nearPosition] = big;
         }
         this.array = sortedArray;
     }
-
-
 }
